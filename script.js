24b18e5591e8bebc6bcc104ec00bdfd049123203
gunnergunville <57113071+gunnergunville@users.noreply.github.com>
2021-04-13 10:58:21 -0400

    Update faq.html

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mobilehamburger() {
   let icon = document.getElementById("hamburger");
 
   if (mainTopnav.className === "topnav") {
-    mainTopnav.className += "clicked";
+    mainTopnav.className += " clicked";
     console.log ('icon clicked');
     icon.src="close.svg";
   }
@@ -12,8 +12,5 @@ function mobilehamburger() {
     mainTopnav.className = "topnav";
     icon.src="hamburger.svg";
 
-
-
-  
   }
 }
